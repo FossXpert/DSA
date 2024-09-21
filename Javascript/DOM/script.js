d0266ea09d12473aa87bb71a189c3150cae9e650
FossXpert <FossXpert@outlook.com>
2024-09-22 01:42:22 +0530

    Refactor HTML, CSS, and JavaScript files

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -1,3 +1,9 @@
 
-var a = document.querySelector("#h1")
-console.log(a);
+var a = document.querySelectorAll("h1")
+a.forEach(element => {
+    console.log(element)
+    element.style.color = 'black'
+    element.style.backgroundColor = 'teal'
+
+    element.innerText = 'hi man'
+});
