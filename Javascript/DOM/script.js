d5d7acd25c502a7c775fe0461ef2f8520f4cfcc3
FossXpert <FossXpert@outlook.com>
2024-09-24 18:08:55 +0530

    Refactor HTML, CSS, and JavaScript files

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -1,9 +1,32 @@
 
 var a = document.querySelectorAll("h1")
 a.forEach(element => {
-    console.log(element)
     element.style.color = 'black'
     element.style.backgroundColor = 'teal'
+    element.innerText = 'Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation'
+});
 
-    element.innerText = 'hi man'
+
+// Example of event Capturing or trickling
+
+const parent = document.getElementById('parent')
+const parent1 = document.getElementById('parent1')
+const parent2 = document.getElementById('parent2')
+const parent3 = document.getElementById('parent3')
+const child = document.getElementById('child')
+
+parent.addEventListener('click',function(){
+    console.log('parent')   
+},true);
+parent1.addEventListener('click',function(){
+    console.log('parent1')   
+},true);
+parent2.addEventListener('click',function(){
+    console.log('parent2')   
+},true);
+parent3.addEventListener('click',function(){
+    console.log('parent3')   
+});
+child.addEventListener('click',function(){
+    console.log('child')   
 });
