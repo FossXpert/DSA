557fa07c3053233625003d3a056f0dc92a2335b8
FossXpert <FossXpert@outlook.com>
2024-09-25 11:46:18 +0530

    Refactor HTML, CSS, and JavaScript files

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -9,24 +9,32 @@ a.forEach(element => {
 
 // Example of event Capturing or trickling
 
-const parent = document.getElementById('parent')
-const parent1 = document.getElementById('parent1')
-const parent2 = document.getElementById('parent2')
-const parent3 = document.getElementById('parent3')
-const child = document.getElementById('child')
+// const parent = document.getElementById('parent')
+// const parent1 = document.getElementById('parent1')
+// const parent2 = document.getElementById('parent2')
+// const parent3 = document.getElementById('parent3')
+// const child = document.getElementById('child')
 
-parent.addEventListener('click',function(){
-    console.log('parent')   
-},true);
-parent1.addEventListener('click',function(){
-    console.log('parent1')   
-},true);
-parent2.addEventListener('click',function(){
-    console.log('parent2')   
-},true);
-parent3.addEventListener('click',function(){
-    console.log('parent3')   
-});
-child.addEventListener('click',function(){
-    console.log('child')   
-});
+// parent.addEventListener('click',function(){
+//     console.log('parent')   
+// },true);
+// parent1.addEventListener('click',function(){
+//     console.log('parent1')   
+// },true);
+// parent2.addEventListener('click',function(){
+//     console.log('parent2')   
+// },true);
+// parent3.addEventListener('click',function(){
+//     console.log('parent3')   
+// });
+// child.addEventListener('click',function(){
+//     console.log('child')   
+// });
+
+//Event delegation example
+
+const parent = document.getElementById('parent');
+
+parent.addEventListener('click',(e)=>{
+    console.log(e.target);
+})
