44689787cadba0354f19b89bf389f037e6af9560
FossXpert <FossXpert@outlook.com>
2024-10-10 18:15:16 +0530

    Refactor currying-infinite-currying.js to optimize code and improve performance

--- Javascript/DOM/Leetcode-JS/currying-infinite-currying.js
+++ Javascript/DOM/Leetcode-JS/currying-infinite-currying.js
@@ -1,12 +1,20 @@
 function add(a){
-    console.log("a",a);
-    return function(b){
-        return function(c){
-            return a+b+c;
-        }
+    
+    let sum = a;
+
+    function inner(b){
+        sum += b;
+        return inner; 
     }
+
+    inner.valueOf = function(){
+        return sum; 
+    }
+    return inner;
 }
 
-const ans = add(2)(3)(4)()
-console.log(ans);
+const ans = add(2)(3)(4)(3)(6)(7)(8)(9)(1)(2)(3)(4)(5)(6)
+console.log(ans+0);
+
+// Approach for interview:
 
