1ffcc39627265be975bb81ea69eb930ffe420e2c
Rahul Ray <FossXpert@outlook.com>
2024-10-14 14:13:16 +0000

    Implement .bind() polyfill

---
+++ Javascript/DOM/Leetcode-JS/polyfill/call-apply-bind-polyfil.js
@@ -0,0 +1,36 @@
+//.bind() polyfill (Write this in interview) watch Akshay Saini Video
+
+
+//Normal .bind() working syntax
+
+const userName = {
+    firstName: 'Rahul',
+    lastName: 'Ray',
+    job: 'Y',
+    company: 'Infosys'
+}
+
+function getDetails(smart, hardworking) {
+    console.log(this.firstName + " " + this.lastName + " " + `who works in ${this.company}, He is ` + smart + " " + hardWorking+" guy")
+}
+
+let getUser = getDetails.bind(userName, 'smart', 'hardWorking');
+console.log(getUser)
+getUser()
+
+
+
+// Now we have to implement a custom myBind() method who works exactly like .bind()
+
+
+let getCustomUser = getDetails.myBind(userName);
+
+Function.prototype.myBind = function (...args) {
+    let obj = args[0]
+    return function () {
+
+    }
+}
+
+
+
