d49c92e77819d26168abccd207da288174114e37
Rahul Ray <FossXpert@outlook.com>
2024-10-14 08:08:47 +0000

    Implement customFlatArray function for flattening arrays with a specified depth

--- Javascript/DOM/Leetcode-JS/Implement-Array-Flat-Function.js
+++ Javascript/DOM/Leetcode-JS/Implement-Array-Flat-Function.js
@@ -5,3 +5,23 @@ const arr = [1, [2, 3], [4, [5, 6]], [7, [8, [9, 10]]]];
 
 console.log(arr.flat(3))
 
+
+
+function customFlatArray(arr,depth=1){
+
+    let result = []
+    arr.forEach(element => {
+        if(Array.isArray(element) === true && depth >0){
+            result  = result.concat(customFlatArray(element,depth-1))
+            console.log("resulthere",result);
+        }else{
+            result.push(element);
+            console.log("result1",result)
+        }
+    });
+
+    return result;
+
+}
+
+console.log(customFlatArray(arr,3))
