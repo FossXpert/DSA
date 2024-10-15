577176760ec65a415c07c623890e6839fa342238
FossXpert <FossXpert@outlook.com>
2024-10-15 17:10:27 +0530

    Implement custom myBind() method for function invocation

--- Javascript/DOM/Leetcode-JS/polyfill/bind-polyfil.js
+++ Javascript/DOM/Leetcode-JS/polyfill/bind-polyfil.js
@@ -20,24 +20,38 @@ getUser();
 
 
 //Final ANswer
-//First watch thisd vide 
+//First watch thisd video to understand then u can only understand 2nd implememtation
 //https://youtu.be/ke_y6z0xRpk?si=ZDNZrdTnMeiibSwd
 // Now we have to implement a custom myBind() method who works exactly like .bind()
 
-Function.prototype.myBind = function (...args) {
+Function.prototype.myBind = function (...args1) {
     let obj = this;
-    let params = args.splice(1);
+    let params = args1.splice(1);
     console.log("params",params);
     return function (...args2) {
         // ..args2 are those arguments which are being passed directly from getUser2()
         let combineArgs = [...params,...args2] //using ES6 Syntax, It creates a larger array
         let combineArgs1 = params.concat(...args2) // using normal concat method
-        obj.apply(args[0],combineArgs1);
+        obj.apply(args1[0],combineArgs1);
     }
 }
 
-let getUser2 = getDetails.myBind(userName,'Smart');
+
+// 2nd and easy implementations yaad rkhna
+//https://youtu.be/K7Ur441AFy4?si=MibM6Vh2OPxKyuUn
+
+Function.prototype.myBind1 = function(obj={}, ...args1){
+
+    obj.fn = this
+    return function(...args2){
+        obj.fn(...args1,...args2)
+    }
+}
+
+let getUser2 = getDetails.myBind1(userName,'Smart');
 getUser2("Nice");
+
+
 
 
 
--- Javascript/DOM/Leetcode-JS/polyfill/call-polyfil.js
+++ Javascript/DOM/Leetcode-JS/polyfill/call-polyfil.js
@@ -1,4 +1,5 @@
 // Here first we see how the call() methods works
+//https://youtu.be/K7Ur441AFy4?si=MibM6Vh2OPxKyuUn
 
 const userName = {
     firstName: 'Rahul',
@@ -11,10 +12,33 @@ function getUserData(smart,hardWorking){
     console.log(this.firstName + " " + this.lastName + " " + `who works in ${this.company}, He is ` + smart + ` and ${hardWorking} ` +"guy")
 }
 
-getUserData.call(userName);
+getUserData.call(userName,'smart','powerfull');
 
 //We have to create polyfill for that.
+//here is the polyfill for call()
+
+Function.prototype.myCall = function(obj={} , ...args){
+    
+    if(typeof this !== 'function'){
+        throw new Error("Not a callable function"  )
+    }
+    
+    obj.fn = this;
+    obj.fn(...args)
+}
+
+getUserData.myCall(userName,'smart','powerfull');
 
 
 
+//here is the polyfill for apply()
 
+Function.prototype.myApply = function(obj={}, arg = []){
+
+    if(!Array.isArray(arg)){
+        throw new Error("CreateListFrom Array like called on object")
+    }
+    obj.fn = this;
+    obj.fn(...arg);
+}
+getUserData.myApply(userName,['smart','powerfull']);
